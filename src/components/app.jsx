be3fcb8fba51ebda14f1004c74e39a24a5a66d48
Anderson Chen <ander.chen00@gmail.com>
2018-12-11 19:13:53 -0200

    Once click on a gif from the list, it returnsthe gif itself

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import giphy from 'giphy-api';
-// const GIPHY_API_KEY = '1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm';
 
 import SearchBar from './searchbar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
+
+const GIPHY_API_KEY = '1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm';
 
 class App extends Component {
   constructor(props) {
@@ -12,15 +13,15 @@ class App extends Component {
 
     this.state = {
       gifs: [],
-      selectedGifId: "yYSSBtDgbbRzq"
+      selectedGifId: 'yYSSBtDgbbRzq'
     }
   }
 
   search = (query) => {
-    giphy('1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm').search({
+    giphy({GIPHY_API_KEY}).search({
         q: query,
         rating: 'g',
-        limit: 10
+
     }, (error, result) => {
       this.setState({
         gifs: result.data
@@ -28,11 +29,13 @@ class App extends Component {
     });
   }
 
-  render() {
-     const gifs =[
-        { id: "yYSSBtDgbbRzq"}
-     ];
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
 
+  render() {
     return (
       <div>
         <div className="left-scene">
@@ -42,7 +45,7 @@ class App extends Component {
           </div>
         </div>
         <div className="right-scene">
-          <GifList gifs = {this.state.gifs}/>
+          <GifList gifs = {this.state.gifs} selectGif={this.selectGif} />
         </div>
       </div>
     );
--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,10 +1,17 @@
 import React, { Component} from 'react';
 
 class Gif extends Component {
+
+  handleClick = () => {
+    if(this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
   render() {
-    const src = `https://media0.giphy.com/media/${this.props.id}/200.webp?cid=3640f6095c0ff72335527a3967664eda`
+    const src = `https://media0.giphy.com/media/${this.props.id}/200.gif`
     return(
-      <img src={src} alt="" className="gif"/>
+      <img src={src} alt="" className="gif" onClick={this.handleClick} />
     );
   }
 }
